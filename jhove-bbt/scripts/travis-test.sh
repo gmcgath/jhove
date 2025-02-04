003149fa1ced2cfc0fe741d1d3629f19c7a5e804
Carl Wilson <carl@openpreservation.org>
2025-02-04 18:37:19 +0000

    FIX: Patched issue with Maven colorised output.

--- jhove-bbt/scripts/travis-test.sh
+++ jhove-bbt/scripts/travis-test.sh
@@ -54,7 +54,8 @@ fi
 [[ -d "${TARGET_ROOT}" ]] || mkdir -p "${TARGET_ROOT}"
 
 # Grab the Major and Minor versions from the full Maven project version string
-MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -f jhove-installer/pom.xml)
+#MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -f jhove-installer/pom.xml)
+MVN_VERSION="1.32.1"
 MAJOR_MINOR_VER="${MVN_VERSION%.*}"
 JHOVE_INSTALLER="./jhove-installer/target/jhove-xplt-installer-${MVN_VERSION}.jar"
 
